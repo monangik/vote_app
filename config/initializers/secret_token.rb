83992594bdd792faa772feeef643807edb7f3c7b
Soham Das <monangik@gmail.com>
2012-05-18 23:17:38 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoteApp::Application.config.secret_token = '07baec019db8655b49f265aaa4ae6bbf4d23ea49a4e037ef940a6140704924a0872411e45760d89bb7bacb61ac89947085ea2adc08977c0f201cf286949ca9a1'
